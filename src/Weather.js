7b1e6658aac2868ca3288117c69d473ba14b1d6e
taymitsu <tmitsumori96@gmail.com>
2023-01-25 21:08:55 -0800

    useState for zipcode implemented

--- src/Weather.js
+++ src/Weather.js
@@ -2,11 +2,16 @@ import { useState } from 'react';
 import './Weather.css'
 
 function Weather() {
+
+  const[ zip, setZip ] = useState('')
   return (
     <div className="Weather">
+      <h1>{zip}</h1>
       <form>
         <input 
           placeholder="Enter Zipcode"
+          value={zip}
+          onChange={e => setZip(e.target.value)}
         />
         <button>Submit</button>
       </form>

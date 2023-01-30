c21f0dac17364abdf5d7a86014d96c1621b5280d
taymitsu <tmitsumori96@gmail.com>
2023-01-30 15:41:47 -0800

    zip and unit included in form

--- src/Weather.js
+++ src/Weather.js
@@ -3,17 +3,29 @@ import './Weather.css'
 
 function Weather() {
 
-  const[ zip, setZip ] = useState('')
+  const[zip, setZip] = useState(' ')
+  const[unit, setUnit] = useState(' ')
+
   return (
     <div className="Weather">
-      <h1>{zip}</h1>
+      <h1>{zip} {unit}</h1>
       <form>
+        <div>
         <input 
           placeholder="Enter Zipcode"
           value={zip}
           onChange={e => setZip(e.target.value)}
         />
+
         <button>Submit</button>
+        </div>
+
+        <select onChange={e => setUnit(e.target.value)}>
+          <option value="metric">Celcius</option>
+          <option value="imperial">Fahrenheit</option>
+          <option value="standard">Kelvin</option>
+        </select>
+        
       </form>
     </div>
   )

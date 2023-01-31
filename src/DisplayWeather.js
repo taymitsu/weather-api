dfad9aec967c5ee9cf961ac46e953d398b6583a3
taymitsu <tmitsumori96@gmail.com>
2023-01-31 15:44:07 -0800

    error message displaying

--- src/DisplayWeather.js
+++ src/DisplayWeather.js
@@ -1,5 +1,12 @@
 function DisplayWeather(props) {
-  const { temp, feelsLike, desc } = props
+  const { temp, feelsLike, desc, cod, message } = props
+
+  //outside of return statement, cod/message
+  if (cod !== 200) {
+    return (
+      <small>{message}</small>
+    )
+  }
   
   return(
     <div className="WeatherDisplay">
--- src/Weather.js
+++ src/Weather.js
@@ -18,7 +18,15 @@ function Weather() {
     const res = await fetch(path)
     const json = await res.json()
 
-    console.log(json)
+    //console.log(json)
+    const cod = json.cod
+    const message = json.message
+
+    if (cod !== 200) {
+      setData({ cod, message })
+      return
+    }
+
     
     const temp = json.main.temp
     const feelsLike = json.main.feels_like
@@ -26,6 +34,8 @@ function Weather() {
 
     //{...data} calls this 
     setData({
+      cod,
+      message,
       temp,
       feelsLike,
       desc
